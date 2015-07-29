1e24cd7e780a9e160bfe4679c493b90fddd3230c
Louis Simpson <louie@Louiss-MacBook-Pro.local>
2015-07-29 14:20:40 -0700

    Code Review

--- src/consulstack.py
+++ src/consulstack.py
@@ -18,7 +18,7 @@ Options:
 from environmentbase.networkbase import NetworkBase
 from environmentbase.cli import CLI
 from environmentbase.template import Template
-from troposphere import ec2, Tags, Ref, iam, GetAtt, Join, FindInMap, Output
+from troposphere import ec2, Tags, Ref, iam, GetAtt, Join, FindInMap, Output, Select
 from troposphere.ec2 import NetworkInterfaceProperty
 from troposphere.sqs import Queue
 from troposphere.autoscaling import Tag
@@ -28,6 +28,8 @@ import troposphere.autoscaling as autoscaling
 import troposphere.elasticloadbalancing as elb
 from environmentbase import resources
 from environmentbase.patterns import bastion
+import template_utils as utils
+
 
 class ConsulTemplate(Template):
     """
@@ -42,7 +44,9 @@ class ConsulTemplate(Template):
     # default configuration values
     DEFAULT_CONFIG = {
         'consul': {
-            'ami_id': 'amazonLinuxAmiId'
+            'consul_ami_id': 'amazonLinuxAmiId',
+            'nat_ami_id': 'natAmiId'
+
         }
     }
 
@@ -92,11 +96,186 @@ class ConsulTemplate(Template):
     # - self.subnets: keyed by type and index (e.g. self.subnets['public'][1])
     # - self.azs: List of parameter references
     def build_hook(self):
-        self.create_consul_cluster(self.ami_id)
+        self.create_consul_cluster()
 
 
-    def create_consul_cluster(self, ami_id):
-        self.add_resource(ec2.Instance("ec2instance", InstanceType="m3.medium", ImageId=ami_id) )
+    def create_consul_cluster(self):
+
+        vpc = Ref(self.vpc_id)
+        #import pdb; pdb.set_trace()
+        public_network_acl = utils.create_network_acl(self, 'PublicNetworkAcl', vpc)
+
+        utils.create_network_acl_entry(
+            self, 'InboundHTTPPublicNetworkAclEntry',
+            public_network_acl, 100, (80, 80))
+
+        utils.create_network_acl_entry(
+            self, 'InboundHTTPSPublicNetworkAclEntry',
+            public_network_acl, 101, (443, 443))
+
+        utils.create_network_acl_entry(
+            self, 'InboundSSHPublicNetworkAclEntry',
+            public_network_acl, 102, (22, 22))
+
+        utils.create_network_acl_entry(
+            self, 'InboundEphemeralPublicNetworkAclEntry',
+            public_network_acl, 103, (1024, 65535))
+
+        utils.create_network_acl_entry(
+            self, 'OutboundPublicNetworkAclEntry',
+            public_network_acl, 100, (0, 65535), protocol=-1, egress=True)
+
+
+        consul_security_group = utils.create_security_group(
+            self, 'ConsulSecurityGroup', 'Enables internal access to Consul', vpc,
+            ingress=[
+                ec2.SecurityGroupRule(
+                    IpProtocol='tcp', CidrIp=utils.WILDCARD_CIDR, FromPort=p, ToPort=p
+                )
+                for p in [22, 53, 8400, 8500, 8600]
+            ] + [
+                ec2.SecurityGroupRule(
+                    IpProtocol=p, CidrIp=utils.WILDCARD_CIDR, FromPort=8300, ToPort=8302
+                )
+                for p in ['tcp', 'udp']
+            ] + [
+                ec2.SecurityGroupRule(
+                    IpProtocol='udp', CidrIp=utils.WILDCARD_CIDR, FromPort=p, ToPort=p
+                )
+                for p in [53, 8400, 8500, 8600]
+            ],
+            egress=[
+                ec2.SecurityGroupRule(
+                    IpProtocol='tcp', CidrIp=utils.WILDCARD_CIDR, FromPort=p, ToPort=p
+                )
+                for p in [53, 80, 443, 8400, 8500, 8600]
+            ] + [
+                ec2.SecurityGroupRule(
+                    IpProtocol=p, CidrIp=utils.WILDCARD_CIDR, FromPort=8300, ToPort=8302
+                )
+                for p in ['tcp', 'udp']
+            ] + [
+                ec2.SecurityGroupRule(
+                    IpProtocol='udp', CidrIp=utils.WILDCARD_CIDR, FromPort=p, ToPort=p
+                )
+                for p in [53, 8400, 8500, 8600]
+            ]
+        )
+
+        private_network_acl = utils.create_network_acl(self, 'PrivateNetworkAcl', vpc)
+
+        utils.create_network_acl_entry(
+            self, 'InboundPrivateNetworkAclEntry',
+            private_network_acl, 100, (0, 65535), protocol=-1)
+
+        utils.create_network_acl_entry(
+            self, 'OutBoundPrivateNetworkAclEntry',
+            private_network_acl, 100, (0, 65535), protocol=-1, egress=True)
+
+        # public_subnets = []
+        private_subnets = []
+
+        for index in range(len(self.azs)):
+
+            public_subnet = self.subnets['public'][index + 1]    
+
+            # # Public Subnet
+            # public_subnet = utils.create_subnet(
+            #     self, 'PublicSubnet%s' % index, vpc,
+            #     '10.0.%s.0/24' % index,
+            #     Join('', [Ref('AWS::Region'), Select(index, Ref(self.azs))]),
+            # )
+
+            # Public Subnet Associations
+            self.add_resource(ec2.SubnetRouteTableAssociation(
+                '%sPublicRouteTableAssociation' % public_subnet.title,
+                SubnetId=Ref(public_subnet),
+                RouteTableId=Ref(public_route_table)
+            ))
+
+            self.add_resource(ec2.SubnetNetworkAclAssociation(
+                '%sPublicSubnetNetworkAclAssociation' % public_subnet.title,
+                SubnetId=Ref(public_subnet),
+                NetworkAclId=Ref(public_network_acl)
+            ))
+
+            # NAT Device(s) are placed in the public subnet(s)
+natAmiId
+            name = 'NATDevice%s' % (index + 1)
+            nat_device = self.add_resource(ec2.Instance(
+                name,
+                InstanceType=Ref(natAmiId),
+                KeyName=Ref(keyname_param),
+                SourceDestCheck=False,
+                ImageId=FindInMap('RegionMap', Ref('AWS::Region'), self.ami_id),
+                NetworkInterfaces=[
+                    ec2.NetworkInterfaceProperty(
+                        Description='ENI for NAT device',
+                        GroupSet=[Ref(nat_security_group)],
+                        SubnetId=Ref(public_subnet),
+                        PrivateIpAddress='10.0.%s.4' % index,
+                        AssociatePublicIpAddress=True,
+                        DeviceIndex=0,
+                        DeleteOnTermination=True,
+                    )
+                ],
+                Tags=Tags(Name=name)
+            ))
+
+            # Private Subnet
+
+
+
+            private_subnet = self.subnets['private'][index + 1]
+
+            #private_subnet =  utils.create_subnet(
+            #     self, 'PrivateSubnet%s' % index, vpc,
+            #     '10.0.%s.0/20' % (16 + 16 * index),
+            #     Join('', [Ref('AWS::Region'), Select(index, Ref(self.azs))])
+            # )
+
+            private_route_table = utils.create_route_table(
+                self, 'PrivateRouteTable%s' % (index + 1), vpc)
+
+            # Route all outbound traffic to the NAT
+            private_route = utils.create_route(
+                self, 'PrivateRoute%s' % (index + 1), private_route_table,
+                InstanceId=Ref(nat_device))
+
+            self.add_resource(ec2.SubnetRouteTableAssociation(
+                '%sPrivateSubnetRouteTableAssociation' % private_subnet.title,
+                SubnetId=Ref(private_subnet),
+                RouteTableId=Ref(private_route_table)
+            ))
+
+            self.add_resource(ec2.SubnetNetworkAclAssociation(
+                '%sPrivateSubnetNetworkAclAssociation' % private_subnet.title,
+                SubnetId=Ref(private_subnet),
+                NetworkAclId=Ref(private_network_acl)
+            ))
+
+            # Consul servers go in the private subnet
+            name = 'ConsulHost%s' % (index + 1)
+            consul_host = self.add_resource(ec2.Instance(
+                name,
+                InstanceType=Ref(consul_instance_type_param),
+                KeyName=Ref(keyname_param),
+                ImageId=FindInMap('RegionMap', Ref('AWS::Region'), self.ami_id),
+                NetworkInterfaces=[
+                    ec2.NetworkInterfaceProperty(
+                        Description='ENI for Consul host',
+                        GroupSet=[Ref(consul_security_group)],
+                        SubnetId=Ref(private_subnet),
+                        PrivateIpAddress='10.0.%s.4' % (16 + 16 * index),
+                        DeviceIndex=0,
+                        DeleteOnTermination=True,
+                    )
+                ],
+                Tags=Tags(Name=name)
+            ))
+
+            #public_subnets.append(public_subnet)
+            private_subnets.append(private_subnet)
 
 
 class ConsulStackController(NetworkBase):
@@ -143,6 +322,7 @@ class ConsulStackController(NetworkBase):
 
         # Create our ELK Template (defined above)
         consul_template = ConsulTemplate(env_name, consul_config.get('ami_id'))
+        #import pdb; pdb.set_trace()
 
         # Add the ELK template as a child of the top-level template
         # Note: This function modifies the incoming child template by attaching some standard inputs. For details
