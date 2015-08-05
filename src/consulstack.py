a54142cb25c92c64d20b5902d65fd5dacdc0c4fd
Louis Simpson <louie@Louiss-MacBook-Pro.local>
2015-08-04 22:30:28 -0700

    Beta

--- src/consulstack.py
+++ src/consulstack.py
@@ -19,10 +19,8 @@ from environmentbase.networkbase import NetworkBase
 from environmentbase.cli import CLI
 from environmentbase.template import Template
 from environmentbase import template
-from troposphere import ec2, Tags, Ref, iam, GetAtt, Join, FindInMap, Output, \
-Select, Base64
-from troposphere.ec2 import NetworkInterfaceProperty
-import troposphere.iam as iam
+from troposphere import ec2, Tags, Ref, iam, GetAtt, Join, \
+Select, Base64, FindInMap, Output
 import boto.vpc
 import boto
 import json
@@ -33,47 +31,28 @@ from pprint import pprint
 
 class ConsulTemplate(Template):
 
-
-    DEFAULT_POLICY = iam.Policy(
-        PolicyName='cloudformationRead',
-        PolicyDocument={
-            "Statement": [{
-                  "Effect": "Allow",
-                  "Action": [
-                      "cloudformation:DescribeStackEvents",
-                      "cloudformation:DescribeStackResource",
-                      "cloudformation:DescribeStackResources",
-                      "cloudformation:DescribeStacks",
-                      "cloudformation:ListStacks",
-                      "cloudformation:ListStackResources"],
-                  "Resource": "*"}]})
-    # Load USER_DATA scripts from package
-    BOOTSTRAP_SH = resources.get_resource('consul_bootstrap.sh', __name__)
-
     # When no config.json file exists a new one is created using the 'factory default' file.  This function
     # augments the factory default before it is written to file with the config values required by an ConsulTemplate
     @staticmethod
     def get_factory_defaults():
         return {'consul': {
-            'ami_id': 'ubuntu1404LtsAmiId',
+            's_ami_id': 'ubuntu1404LtsAmiId',
+            'a_ami_id': 'sampleServiceAmiId'
         }}
 
-    # When the user request to 'create' a new ELK template the config.json file is read in. This file is checked to
-    # ensure all required values are present. Because ELK stack has additional requirements beyond that of
-    # EnvironmentBase this function is used to add additional validation checks.
     @staticmethod
     def get_config_schema():
         return {'consul': {
-            'ami_id': 'str'
+            's_ami_id': 'str',
+            'a_ami_id': 'str'
         }}
 
-    
-
-    # Collect all the values we need to assemble our ELK stack
-    def __init__(self, env_name, ami_id, boto_config={}):
+    # Collect all the values we need to assemble our Consul.io stack
+    def __init__(self, env_name, s_ami_id, a_ami_id, boto_config={}):
         super(ConsulTemplate, self).__init__('ConsulStack')
         self.env_name = env_name
-        self.ami_id = ami_id
+        self.s_ami_id = s_ami_id
+        self.a_ami_id = a_ami_id
         self.boto_config = boto_config
     # Called after add_child_template() has attached common parameters and some instance attributes:
     # - RegionMap: Region to AMI map, allows template to be deployed in different regions without updating AMI ids
@@ -103,40 +82,47 @@ class ConsulTemplate(Template):
 
         startup_vars = []
         startup_vars.append(Join('=', ['REGION', Ref('AWS::Region')]))
-        startup_vars.append(Join('=', ['UI', '/var/consul/ui']))
-        startup_vars.append(Join('=', ['DATA', '/var/consul/data']))
-        startup_vars.append(Join('=', ['JOIN', '"10.0.16.4"']))
-        startup_vars.append(Join('=', ['CONFIG', '/etc/consul']))
         startup_vars.append(Join('=', ['AZCOUNT', len(self.azs)]))
 
+        #Array to hold predefined IPs for our consul host
         p_ids = []
 
         for index in range(len(self.azs)):
             p_ids.append("10.0.%s.4" % ((16 + 16) * (index+1)))
 
-        #Get our server config strings
-        p_id_str = ','.join(map(str,p_ids))
 
+        delim = '\n'
         #Grab our consul.conf file to distribute to nodes
         with open('templates/consul.json') as json_file:
             json_data = json.load(json_file)
 
 
+        #Loop over the AZs for our region
         for index in range(len(self.azs)):
-            name = 'ConsulHost%s' % (index + 1)
+            cname = 'ConsulHost%s' % (index + 1)
+            lname = 'ExampleLampStack%s' % (index + 1)
 
-            #p_id = '10.0.%s.4' % ((16 + 16) * (index+1))
+            #Add in our consul defaults for the various configs we need
+            #to write to our nodes
+            json_data['server'] = True
             json_data['start_join'] = p_ids
             json_data['data_dir'] = '/var/consul/data'
             json_data['ui_dir'] = '/var/consul/ui'
-            json_data['datacenter'] = 'us-west-2'
+            json_data['datacenter'] = "dc1"
             json_data['encrypt'] = 'Z+QYrQRxLf/RgFL64dnCNA=='
+            #startup_vars.append(Join('=', ['IP', p_ids[index]]))
 
+            #If the index is 0 we are at our bootstrap machine.
+            #Clear out start_join param and set bootstrap_expect
+            #to the number of AZs
             if(index == 0):
                 json_data['bootstrap_expect'] = len(self.azs)
                 if(json_data.get('start_join')):
                     del json_data['start_join']
 
+            #This is reg node in the cluster
+            #Remove bootstrap config
+            #add join config for nodes excluding their own IP
             else:
                 ohosts = []
                 if(json_data.get('bootstrap_expect')):
@@ -148,22 +134,12 @@ class ConsulTemplate(Template):
                         ohosts.append(i)
                 json_data['start_join'] = ohosts
 
-            print json.dumps(json_data).strip()
-            pprint(json_data)
-            # pprint(boto_config.get('region_name', 'us-west-2'))
-            # region = template.tropo_to_string(Ref('AWS::Region'))
-            # import pdb; pdb.set_trace()
-            #print template.tropo_to_string(self)
-            #print template.tropo_to_string(self.subnets['private'][index])
-            #print "Configuring {0} with security group {1}".format(name, consul_security_group)
-            #print "Configuring {0} with security group {1}".format(name, self.common_security_group)
+
             consul_host = self.add_resource(ec2.Instance(
-                name,
+                cname,
                 InstanceType="m1.small",
                 KeyName=Ref(self.parameters['ec2Key']),
-                ImageId=FindInMap('RegionMap', Ref('AWS::Region'), self.ami_id),
-                #SecurityGroups=[Ref(consul_security_group)],
-                #AvailabilityZone=Ref(self.azs[index]),
+                ImageId=FindInMap('RegionMap', Ref('AWS::Region'), self.s_ami_id),
                 NetworkInterfaces=[
                     ec2.NetworkInterfaceProperty(
                         Description='ENI for CONSUL hosts',
@@ -176,25 +152,13 @@ class ConsulTemplate(Template):
                 ],
                 UserData=Base64(Join('', [
                     '#!/bin/bash\n\n',
-                    #'sudo apt-get update\n',
-                    #'sudo apt-get -y install python-setuptools\n',
-                    #'sudo apt-get -y install python-pip\n',
+                    'sudo apt-get update\n',
                     'sudo apt-get -y install unzip\n',
                     'sudo apt-get -y install dnsmasq\n',
-                    #'sudo wget -P /root https://s3.amazonaws.com/cloudformation'
-                    #'-examples/aws-cfn-bootstrap-latest.tar.gz\n',
-                    #'sudo mkdir -p /root/aws-cfn-bootstrap-latest\n',
-                    #'sudo tar xvfz /root/aws-cfn-bootstrap-latest.tar.gz ',
-                    #'--strip-components=1 -C /root/aws-cfn-bootstrap-latest\n',
-                    #'sudo easy_install /root/aws-cfn-bootstrap-latest/\n',
-                    #'sudo cfn-init -s \'', Ref('AWS::StackName'),
-                    #'\' -r Ec2Instance -c ascending --region \'',
-                    #Ref('AWS::Region'),
                     'sudo mkdir -p /var/consul\n',
                     'sudo mkdir -p /var/consul/ui\n',
                     'sudo mkdir -p /var/consul/data\n',
                     'sudo mkdir -p /etc/consul\n',
-
                     'sudo wget -O /tmp/consul.zip https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip\n',
                     'sudo wget -O /tmp/consul-ui.zip https://dl.bintray.com/mitchellh/consul/0.5.2_web_ui.zip\n',
                     'sudo unzip -n -d /bin /tmp/consul.zip\n',
@@ -205,40 +169,63 @@ class ConsulTemplate(Template):
                     '\nEOM\n',
                     'sudo cat > /etc/init/consul-server.conf << EOM\n'
                     'description "Consul server service"\n',
-                    'start on runlevel [2345]\n',
-                    'stop on runlevel [06]\n\n',
-                    'exec consul agent -server -data-dir=/var/consul/data -config-dir=/etc/consul\n'
+                    'start on (local-filesystems and net-device-up IFACE=eth0)\n',
+                    'stop on runlevel [!12345]\n\n',
+                    'respawn\n\n',
+                    'exec consul agent -server -config-dir=/etc/consul -atlas louissimps/consul ',
+                    '-atlas-token tHBDcVy0rmG2yg.atlasv1.zhXARgdb1N4RznU56mCrFrVkKwhEyP9E1QLVUiwa3dl',
+                    '8Pa4311MlSkT2jqZ5ckebJws\n',
                     '\nEOM\n',
                     'sudo service consul-server start\n\n'
                 ])),
 
-                #self.build_bootstrap([ConsulTemplate.BOOTSTRAP_SH], variable_declarations=startup_vars),
 
-                Tags=Tags(Name=name, StackName=self.name)
+                Tags=Tags(Name=cname, StackName=self.name)
 
             ))
 
-    def get_cfn_config(self, config):
-        return cloudformation.InitConfig(
+            json_data['start_join'] = [p_ids[index]]
 
-                files=cloudformation.InitFiles({
-                    "/tmp/puppet_agent_init.sh": cloudformation.InitFile(
-                    content=EnvironmentBase.build_bootstrap(
-                        ['user_data/rhel_puppet.sh',
-                         'user_data/rhel_puppet_agent.sh'],
-                        prepend_line='#!/bin/bash -x',
-                        variable_declarations=['SERVER_NAME={}'.format('puppet.'+zone_name)]),
-                    encoding='base64',
-                    mode='000755')
-                }),
+            if(json_data.get('bootstrap_expect')):
+                del json_data['bootstrap_expect']
 
-                commands={
-                    '1_run_puppet_agent_init': {
-                        "cwd": "/tmp",
-                        "command": "./puppet_agent_init.sh"
-                    }
-                }
-            )
+            json_data["server"] = False
+
+            sample_service = self.add_resource(ec2.Instance(
+                lname,
+                InstanceType="m1.small",
+                KeyName=Ref(self.parameters['ec2Key']),
+                SubnetId=self.subnets['private'][index],
+                SecurityGroupIds=[Ref(consul_security_group)],
+                ImageId=FindInMap('RegionMap', Ref('AWS::Region'), self.a_ami_id),
+                UserData=Base64(Join('', [
+                    '#!/bin/bash\n\n',
+                    'sudo apt-get update\n',
+                    'sudo apt-get -y install unzip\n',
+                    'sudo mkdir -p /var/consul\n',
+                    'sudo mkdir -p /var/consul/data\n',
+                    'sudo mkdir -p /etc/consul\n',
+                    'sudo wget -O /tmp/consul.zip https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip\n',
+                    'sudo unzip -n -d /bin /tmp/consul.zip\n',
+                    '\n\n',
+                    'sudo cat > /etc/consul/consul.json << EOM\n',
+                    json.dumps(json_data).strip(),
+                    '\nEOM\n',
+                    'sudo cat > /etc/init/consul-agent.conf << EOM\n'
+                    'description "Consul agent service"\n',
+                    'start on (local-filesystems and net-device-up IFACE=eth0)\n',
+                    'stop on runlevel [!12345]\n\n',
+                    'respawn\n\n',
+                    'exec consul agent -data-dir=/var/consul/data -config-dir=/etc/consul\n'
+                    '\nEOM\n',
+                    'sudo service consul-agent start\n\n',
+
+                ])),
+
+
+                Tags=Tags(Name=lname, StackName=self.name)
+
+            ))
 
 
     def create_consul_sg(self):
@@ -249,7 +236,7 @@ class ConsulTemplate(Template):
                 ec2.SecurityGroupRule(
                     IpProtocol='tcp', FromPort=p, ToPort=p, CidrIp="10.0.0.0/16"
                 )
-                for p in [53, 8400, 8500, 8600]
+                for p in [22, 53, 8400, 8500, 8600]
             ] + [ec2.SecurityGroupRule(
                     IpProtocol=p, FromPort=8300, ToPort=8302, CidrIp="10.0.0.0/16"
                 )
@@ -266,7 +253,7 @@ class ConsulTemplate(Template):
             SecurityGroupIngress= [ec2.SecurityGroupRule(
                     IpProtocol='tcp', FromPort=p, ToPort=p, CidrIp="10.0.0.0/16"
                 )
-                for p in [22, 53, 8400, 8500, 8600]
+                for p in [22, 80, 443, 53, 8400, 8500, 8600]
             ] + [
                 ec2.SecurityGroupRule(
                     IpProtocol=p, FromPort=8300, ToPort=8302, CidrIp="10.0.0.0/16"
@@ -288,10 +275,6 @@ class ConsulStackController(NetworkBase):
     """
     Coordinates CONSUL stack actions (create and deploy)
     """
-    """
-    Coordinates CONSUL stack actions (create and deploy)
-    """
-    #import pdb; pdb.set_trace()
     # When no config.json file exists a new one is created using the 'factory default' file.  This function
     # augments the factory default before it is written to file with the config values required by an ConsulTemplate
     @staticmethod
@@ -315,9 +298,7 @@ class ConsulStackController(NetworkBase):
         self.add_child_template(bastion.Bastion())
         consul_config = self.config.get('consul')
         env_name = self.globals.get('environment_name', 'environmentbase-consul')
-        consul_template = ConsulTemplate(env_name, consul_config.get('ami_id'), boto_config=self.config.get('boto'))
-       # self.validate_cloudformation_template(self.to_json())
-
+        consul_template = ConsulTemplate(env_name, consul_config.get('s_ami_id'), consul_config.get('a_ami_id'), boto_config=self.config.get('boto'))
         self.add_child_template(consul_template)
         self.write_template_to_file()
 
@@ -335,7 +316,7 @@ class ConsulStackController(NetworkBase):
 def main():
     # This cli object takes the documentation comment at the top of this file (__doc__) and parses it against the
     # command line arguments (sys.argv).  Supported commands are create and deploy. The deploy function works fine as
-    # is. ConsulStackController overrides the create action to include an ELK stack as an additional template.
+    # is. ConsulStackController overrides the create action to include an Consul.io stack as an additional template.
     cli = CLI(doc=__doc__)
     ConsulStackController(view=cli)
 
--- src/data/consul_bootstrap.sh
+++ src/data/consul_bootstrap.sh
@@ -1,16 +1,20 @@
-sudo apt-get -y update
-sudo apt-get -y install unzip dnsmasq
+sudo apt-get -y update 
+sudo apt-get -y install unzip 
 
 
-sudo wget -O /tmp/consul.zip https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
-sudo wget -O /tmp/consul-ui.zip https://dl.bintray.com/mitchellh/consul/0.5.2_web_ui.zip
+sudo mkdir -p /etc/consul 
+sudo mkdir -p /var/consul/data 
+
+sudo wget -O /tmp/consul.zip https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip 
 
 
-sudo unzip -n -d /bin /tmp/consul.zip
-sudo unzip -n -d /var/consul/ui /tmp/consul-ui.zip
+sudo unzip -n -d /bin /tmp/consul.zip 
 
-sudo mkdir -p /etc/consul
-sudo mkdir -p /var/consul/data
+sudo mkdir -p /etc/consul 
+sudo mkdir -p /var/consul/data 
+
+
+
 sudo cp templates/consul.json /etc/consul/consul.json
 sudo cp files/consul-server.conf /etc/init/consul-server.conf
 sudo service consul-server start
